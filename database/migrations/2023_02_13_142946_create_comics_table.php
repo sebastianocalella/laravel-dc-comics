f599b9a24fda4535de871e55b00a6c407eee368b
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-13 15:39:24 +0100

    comics table migration

---
+++ database/migrations/2023_02_13_142946_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',120);
+            $table->text('description',2000);
+            $table->float('price',5,2);
+            $table->char('series',100);
+            $table->date('sale_date');
+            $table->char('type',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
