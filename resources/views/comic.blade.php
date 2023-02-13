6d3720172b39b00300ea315d7f81a4bac1f1943b
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-13 16:58:16 +0100

    add comic content

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -2,6 +2,27 @@
 
 @section('main-content')
     <section class="comic">
-        <p>single comic</p>
+        <div class="container">
+            <div class="row">
+                <div class="col-10">
+                    <h3>{{$comic->title}}</h3>
+                </div>
+                <div class="col-2">
+                    <span>{{$comic->price}} &dollar;</span>
+                </div>
+                <div class="col-12">
+                    <p>{{$comic->description}}</p>
+                </div>
+                <div class="col-12">
+                    <p>series: {{$comic->series}}</p>
+                </div>
+                <div class="col-12">
+                    <p>type: {{$comic->type}}</p>
+                </div>
+                <div class="col-12">
+                    <p>sale date: {{$comic->sale_date}}</p>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
