610983ebffc9c554ec487e99f12fca1999edc3ee
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-13 17:45:30 +0100

    add style content to home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,28 @@
 @extends('layouts.app')
 
 @section('main-content')
-    <div class="container">
+    <div class="container-fluid">
+        <div class="container">
         <div class="row">
             <div class="col-12">
-                <p>current series</p>
+                <h2 class="m-3">Our Comics:</h2>
             </div>
-            <div class="comics-container container">
-                <div class="row">
+            <div class="comics-container container mb-5">
+                <div class="row gy-3">
                     @foreach ($comics as $comic)
-                        <div class="col-2">
+                        <div class="col-3">
                             <div class="my-card">
-                                <img src="{{$comic['thumb']}}" alt="comic image">
-                                <p>{{$comic['title']}}</p>
-                                <a href="{{route('comic.show', $comic->id)}}" class="btn btn-sm btn-primary">info</a>
+                                <div class="container-fluid">
+                                    <img src="{{$comic['thumb']}}" alt="comic image" class="w-100">
+                                    <p>{{$comic['title']}}</p>
+                                </div>
+                                <a href="{{route('comic.show', $comic->id)}}" class="btn btn-sm btn-primary mx-3 mb-2">info</a>
                             </div>
                         </div>
                     @endforeach
                 </div>
             </div>
+        </div>
         </div>
     </div>
 @endsection
