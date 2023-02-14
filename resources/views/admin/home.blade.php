e3d38c5fbc40d475731355dc69e125a45f39503f
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-14 14:56:25 +0100

    connected bootstrap to admin page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>DC Comics</title>
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
 </head>
 <body>
-    <h1>admin home</h1>
+    <h1>admin page</h1>
 </body>
 </html>
