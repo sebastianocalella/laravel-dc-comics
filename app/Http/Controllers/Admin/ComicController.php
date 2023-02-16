4d6457227487321e969d40eed4fe86827df3d5cf
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-16 18:55:28 +0100

    Revert "generalized validations in rules() public method"
    This reverts commit 5eda8a2fe860c12eb5b544db1fcb8e8bcb6936c0.

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -9,52 +9,6 @@ use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
-    public function rules(){
-        return [
-            [
-                'title'=>'required|min:3|max:120',
-                'description'=>'required|min:30|max:2000',
-                'thumb'=>'required|max:3000', //maxvalue 11525
-                'price'=>'required|numeric|max:999.99',
-                'series'=>'required|min:3|max:100',
-                'sale_date'=>'required|date',
-                'type'=>'required|max:100'
-            ],
-            [
-                //required alert
-                'title.required'=>'è obbligatoria la compilazione del campo title',
-                'description.required'=>'è obbligatoria la compilazione del campo description',
-                'thumb.required'=>'è obbligatoria la compilazione del campo thumb',
-                'price.required'=>'è obbligatoria la compilazione del campo price',
-                'series.required'=>'è obbligatoria la compilazione del campo series',
-                'sale_date.required'=>'è obbligatoria la compilazione del campo sale_date',
-                'type.required'=>'è obbligatoria la compilazione del campo type',
-
-                //min
-                'title.min'=>'il title deve essere lungo almeno 3 caratteri',
-                'description.min'=>'la description deve essere lunga almeno 30 caratteri',
-                'series.min'=>'series deve essere lungo almeno 3 caratteri',
-
-                //max
-                'title.max'=>'la lunghezza del title non può superare i 120 caratteri',
-                'description.max'=>'la lunghezza della description non può superare i 2.000 caratteri',
-                'thumb.max'=>'la lunghezza del thumb non può superare i 3.000 caratteri',
-                'price.max'=>'il price non può avere un valore superiore ai 999.99$',
-                'series.max'=>'la lunghezza della series non può superare i 100 caratteri',
-                'type.max'=>'la lunghezza del type non può superare i 100 caratteri',
-
-                //numeric
-                'price.numeric'=>'il valore inserito in price deve essere un valore numerico compreso tra 0.00 e 999.99',
-
-                //decimal
-                'price.decimal'=>'il valore dopo la virgola deve essere composto di un massimo di due cifre',
-
-                //date
-                'sale_date.date'=>'il valore inserito in sale_date deve essere di tipo date'
-            ]
-        ];
-    }
-
     /**
      * Display a listing of the resource.
      *
