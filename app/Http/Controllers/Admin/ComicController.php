4e784d0c75c860f13cd9f9efca82635eb5a0447f
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-16 19:04:31 +0100

    try to restore programm

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -36,14 +36,53 @@ class ComicController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(ComicControllerRequest $request)
+    public function store(Request $request)
     {
+        $data = $request->all();
+        $request->validate(
+            [
+            'title'=>'required|min:3|max:120',
+            'description'=>'required|min:30|max:2000',
+            'thumb'=>'required|max:3000', //maxvalue 11525
+            'price'=>'required|numeric|max:999.99',
+            'series'=>'required|min:3|max:100',
+            'series'=>'required|max:100',
+            'sale_date'=>'required|date',
+            'type'=>'required|max:100'
+        ],
+        [
+            //required alert
+            'title.required'=>'è obbligatoria la compilazione del campo title',
+            'description.required'=>'è obbligatoria la compilazione del campo description',
+            'thumb.required'=>'è obbligatoria la compilazione del campo thumb',
+            'price.required'=>'è obbligatoria la compilazione del campo price',
+            'series.required'=>'è obbligatoria la compilazione del campo series',
+            'sale_date.required'=>'è obbligatoria la compilazione del campo sale_date',
+            'type.required'=>'è obbligatoria la compilazione del campo type',
+
+            //min
+            'title.min'=>'il title deve essere lungo almeno 3 caratteri',
+            'description.min'=>'la description deve essere lunga almeno 30 caratteri',
+            'series.min'=>'series deve essere lungo almeno 3 caratteri',
+
+            //max
+            'title.max'=>'la lunghezza del title non può superare i 120 caratteri',
+            'description.max'=>'la lunghezza della description non può superare i 2.000 caratteri',
+            'thumb.max'=>'la lunghezza del thumb non può superare i 3.000 caratteri',
+            'price.max'=>'il price non può avere un valore superiore ai 999.99$',
+            'series.max'=>'la lunghezza della series non può superare i 100 caratteri',
+            'type.max'=>'la lunghezza del type non può superare i 100 caratteri',
+
+            //numeric
+            'price.numeric'=>'il valore inserito in price deve essere un valore numerico compreso tra 0.00 e 999.99',
+
+            //date
+            'sale_date.date'=>'il valore inserito in sale_date deve essere di tipo date'
+        ]);
 
         $newComic = new Comic();
-        $data = $request->validated();
         $newComic->fill($data);
         $newComic->save();
-
         return redirect()->route('admin.comics.show', $newComic->id);
     }
 
@@ -78,10 +117,48 @@ class ComicController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $modelData = $request->validate(ComicController::rules());
+        $modelData = $request->all();
         $comic = Comic::findOrfail($id);
-        $comic->update($modelData);
+        $request->validate([
+            'title'=>'required|min:3|max:120',
+            'description'=>'required|min:30|max:2000',
+            'thumb'=>'required|max:3000', //maxvalue 11525
+            'price'=>'required|numeric|max:999.99',
+            'series'=>'required|min:3|max:100',
+            'sale_date'=>'required|date',
+            'type'=>'required|max:100'
+        ],
+        [
+            //required alert
+            'title.required'=>'è obbligatoria la compilazione del campo title',
+            'description.required'=>'è obbligatoria la compilazione del campo description',
+            'thumb.required'=>'è obbligatoria la compilazione del campo thumb',
+            'price.required'=>'è obbligatoria la compilazione del campo price',
+            'series.required'=>'è obbligatoria la compilazione del campo series',
+            'sale_date.required'=>'è obbligatoria la compilazione del campo sale_date',
+            'type.required'=>'è obbligatoria la compilazione del campo type',
 
+            //min
+            'title.min'=>'il title deve essere lungo almeno 3 caratteri',
+            'description.min'=>'la description deve essere lunga almeno 30 caratteri',
+            'series.min'=>'series deve essere lungo almeno 3 caratteri',
+
+            //max
+            'title.max'=>'la lunghezza del title non può superare i 120 caratteri',
+            'description.max'=>'la lunghezza della description non può superare i 2.000 caratteri',
+            'thumb.max'=>'la lunghezza del thumb non può superare i 3.000 caratteri',
+            'price.max'=>'il price non può avere un valore superiore ai 999.99$',
+            'series.max'=>'la lunghezza della series non può superare i 100 caratteri',
+            'type.max'=>'la lunghezza del type non può superare i 100 caratteri',
+
+            //numeric
+            'price.numeric'=>'il valore inserito in price deve essere un valore numerico compreso tra 0.00 e 999.99',
+
+            //date
+            'sale_date.date'=>'il valore inserito in sale_date deve essere di tipo date'
+        ]);
+
+        $comic->update($modelData);
         return redirect()->route('admin.comics.show', $comic->id);
     }
 
