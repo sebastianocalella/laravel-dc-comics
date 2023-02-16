e806a46b60ab44b5a1b4d63ca63a6626a5290672
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-16 17:51:02 +0100

    try to make request working

--- app/Http/Requests/ComicControllerRequest.php
+++ app/Http/Requests/ComicControllerRequest.php
@@ -1,15 +1,28 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Requests;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\ComicControllerRequest;
-use App\Models\Comic;
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Http\FormRequest;
 
-class ComicController extends Controller
+class ComicControllerRequest extends FormRequest
 {
-    public function rules(){
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
         return [
             [
                 'title'=>'required|min:3|max:120',
@@ -53,93 +66,5 @@ class ComicController extends Controller
                 'sale_date.date'=>'il valore inserito in sale_date deve essere di tipo date'
             ]
         ];
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $comics = Comic::all();
-        return view('admin.home', compact('comics'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('admin.createComic');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(ComicControllerRequest $request)
-    {
-
-        $newComic = new Comic();
-        $data = $request->validated();
-        $newComic->fill($data);
-        $newComic->save();
-
-        return redirect()->route('admin.comics.show', $newComic->id);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Comic $comic)
-    {
-        return view('admin.comic', compact('comic'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Comic $comic)
-    {
-        return view('admin.createComic', ['comic' => $comic]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $modelData = $request->validate(ComicController::rules());
-        $comic = Comic::findOrfail($id);
-        $comic->update($modelData);
-
-        return redirect()->route('admin.comics.show', $comic->id);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Comic $comic)
-    {
-        $comic->delete();
-        return redirect()->route('admin.comics.index');
     }
 }
